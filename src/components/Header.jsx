aef50db2868d4e5f256eaa393633772cac2e0a22
washington gama <wgamaneto16@gmail.com>
2022-10-24 19:59:55 -0300

    inicio da implementacao

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,65 @@
 import React from 'react';
+import { Link, useHistory } from 'react-router-dom';
+
+import SearchBar from './SearchBar';
+import searchIcon from '../images/searchIcon.svg';
+import profileIcon from '../images/profileIcon.svg';
 
 function Header() {
+  const history = useHistory();
+  const [hidden, setHidden] = useState(true);
+
+  const path = () => {
+    switch (history.location.pathname) {
+    case '/meals':
+      return (<h1 data-testid="page-title">Meals</h1>);
+    case '/drinks':
+      return (<h1 data-testid="page-title">Drinks</h1>);
+    case '/profile':
+      return (<h1 data-testid="page-title">Profile</h1>);
+    case '/done-recipes':
+      return (<h1 data-testid="page-title"> Done Recipes </h1>);
+    case '/favorite-recipes':
+      return (<h1 data-testid="page-title">Favorite Recipes</h1>);
+    default: (
+      <h1>Recipes</h1>);
+    }
+  };
+
+  const pathRender = () => {
+    switch (history.location.pathname) {
+    case '/profile':
+      return ('');
+    case '/favorite-recipes':
+      return ('');
+    case '/done-recipes':
+      return ('');
+    default:
+      return (
+        <Link
+          to={ history.location.pathname }
+          data-testid="search-top-btn"
+          src={ searchIcon }
+          onClick={ () => setHidden(!hidden) }
+        >
+          <img src={ searchIcon } alt="profile" />
+        </Link>
+      );
+    }
+  };
   return (
-    <div>Header</div>
+    <header>
+      { path() }
+      { pathRender() }
+      {!hidden && <SearchBar />}
+      <Link
+        data-testid="profile-top-btn"
+        to="/profile"
+        src={ profileIcon }
+      >
+        <img src={ profileIcon } alt="profile" />
+      </Link>
+    </header>
   );
 }
 
--- src/context/RecipeProvider.js
+++ src/context/RecipeProvider.js
@@ -1,11 +1,44 @@
 import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import RecipeContext from './RecipeContext';
 
 function RecipeProvider({ children }) {
-  const history = useHistory();
-  const contextValue = {};
+  const [personalData, setPersonalData] = useState({
+    email: '',
+    password: '',
+  });
+
+  // const [disable, setDisable] = useState({
+  //   isDisabled: false,
+  // });
+
+  const handleChange = ({ target: { name, value } }) => {
+    setPersonalData({ ...personalData, [name]: value });
+  };
+
+  const [filterValue, setFilterValue] = useState({});
+  const [filterType, setFilterType] = useState({});
+  const [mealsData, setMealsData] = useState([]);
+  const [drinkData, setDrinkData] = useState([]);
+  const [toRender, setToRender] = useState([]);
+  const [doneRecipes, setDoneRecipes] = useState([]);
+
+  const contextValue = {
+    filterValue,
+    setFilterValue,
+    filterType,
+    setFilterType,
+    mealsData,
+    setMealsData,
+    drinkData,
+    setDrinkData,
+    toRender,
+    setToRender,
+    doneRecipes,
+    setDoneRecipes,
+    handleChange,
+    personalData,
+  };
 
   return (
     <RecipeContext.Provider value={ contextValue }>

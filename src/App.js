7819bc3520409635dc031fab9399f9b8a0702d82
Alan Foster <alan-af@outlook.com>
2022-10-24 15:47:19 -0300

    tela login

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
+// import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from './Pages/Login';
+import Meals from './Pages/Meals';
 
 function App() {
   return (
-    <div className="meals">
+    <BrowserRouter>
+    <Switch>
+    <Route exact path="/" component={ Login } />
+    <Route exact path="/Meals" component={ Meals } />
+    </Switch>
+    {/* <div className="meals">
       <span className="logo">TRYBE</span>
       <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
+      className="rocksGlass"
+      type="image/svg+xml"
+      data={ rockGlass }
       >
-        Glass
+      Glass
       </object>
-    </div>
+      
+    </div> */}
+    </BrowserRouter>
   );
 }
 

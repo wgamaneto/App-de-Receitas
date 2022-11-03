da80747bda8b34a8004e83c696c803cbfb1fec58
Luan Victor <luanvictor2108@gmail.com>
2022-11-03 18:26:40 -0300

    Fazendo o que deu do recipesInProgress

--- src/Pages/RecipeInProgress.jsx
+++ src/Pages/RecipeInProgress.jsx
@@ -1,7 +1,9 @@
+import clipboardCopy from 'clipboard-copy';
 import React, { useState, useEffect, useCallback } from 'react';
 import { useHistory } from 'react-router-dom';
 import shareIcon from '../images/shareIcon.svg';
-import whireHeartIcon from '../images/whiteHeartIcon.svg';
+import whiteHeartIcon from '../images/whiteHeartIcon.svg';
+import blackHeartIcon from '../images/blackHeartIcon.svg';
 import './RecipeInProgress.css';
 
 function RecipeInProgress() {
@@ -12,22 +14,32 @@ function RecipeInProgress() {
   const [Measures, setMeasures] = useState([]);
   const [recipeSteps, setRecipeSteps] = useState([]);
   const [isDisabled, setIsDisabled] = useState(true);
+  const [linkCopied, setLinkCopied] = useState(false);
+  const [favoriteRecipe, setFavoriteRecipe] = useState([]);
+  const [isFavorite, setIsFavorite] = useState(false);
+  const [isAlchoolic, setIsAlchoolic] = useState('');
   const bars = history.location.pathname.split('/');
   const path = bars[1];
   const fetchRecipeDetails = useCallback(async () => {
-    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${recipeId}`;
-    const data = await fetch(url);
-    const res = await data.json();
-    /* setIsFetched(true); */
-    console.log('entrou no fetch');
-    setRecipeDetails(res);
-  }, [recipeId]);
+    console.log(recipeId);
+    if (path === 'meals') {
+      const data = await
+      fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${recipeId}`);
+      const res = await data.json();
+      setRecipeDetails(res[path]);
+    } else {
+      const data = await
+      fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${recipeId}`);
+      const res = await data.json();
+      setRecipeDetails(res[path]);
+    }
+  }, [recipeId, path]);
   useEffect(() => {
     fetchRecipeDetails();
   }, [fetchRecipeDetails]);
   const getIngredients = useCallback(async () => {
-    if (recipeDetails[path][0] !== undefined) {
-      const entradas = Object.entries(recipeDetails[path][0]);
+    if (recipeDetails.length > 0) {
+      const entradas = Object.entries(recipeDetails[0]);
       const ingredients = entradas.filter(
         (recipe) => recipe[0].includes(
           'strIngredient',
@@ -42,33 +54,39 @@ function RecipeInProgress() {
       const filterMeasure = measureIngredie.map((recipe) => recipe[1]);
       setIngredients(filterIng);
       setMeasures(filterMeasure);
+      if (path === 'drinks') {
+        setIsAlchoolic(recipeDetails[0].strAlcoholic);
+      }
     }
   }, [recipeDetails, path]);
 
-  const initialState = () => {
-    const local = JSON.parse(localStorage.getItem('inRecipeProgress'));
-    console.log(local);
+  const initialState = useCallback(() => {
+    let local = JSON.parse(localStorage.getItem('inRecipeProgress'));
+    if (!local) {
+      local = {
+        meals: {},
+        drinks: {},
+      };
+    }
     if (local[path][recipeId]) {
       setRecipeSteps(local[path][recipeId]);
-
-      local[path][recipeId].forEach((recipe) => {
-        const itens = document.getElementById(recipe);
-        const checkbox = document.getElementsByName(recipe);
+      /* local[path][recipeId].forEach((recipe) => {
+        const itens = document.getElementsByName(recipe);
         console.log(itens);
-        console.log(recipe);
-        itens.className = 'cutted';
+        itens[0].className = 'cutted';
         checkbox[0].checked = true;
-      });
+      }); */
     }
-  };
+  }, [path, recipeId]);
 
   useEffect(() => {
     getIngredients();
     initialState();
-  }, [getIngredients]);
+  }, [getIngredients, initialState]);
 
   useEffect(() => {
-    let local = JSON.parse(localStorage.getItem('inProgressRecipes'));
+    let local = JSON.parse(localStorage.getItem('inRecipeProgress'));
+    console.log(local);
     if (!local) {
       local = {
         meals: {},
@@ -85,154 +103,188 @@ function RecipeInProgress() {
     } else {
       setIsDisabled(true);
     }
-    console.log(recipeSteps.length);
+  };
+
+  const finishRecipe = () => {
+    history.push('/done-recipes');
+    localStorage.setItem('favoriteRecipes', [...favoriteRecipe]);
+  };
+
+  const saveOnFavoriteRecipes = () => {
+    setIsFavorite(!isFavorite);
+    if (path === 'meal') {
+      setFavoriteRecipe([{
+        id: recipeDetails[0].idMeal,
+        type: recipeDetails[0].strTags,
+        nationality: recipeDetails[0].strArea,
+        category: recipeDetails[0].strCategory,
+        alchoolicOrNot: 'Not',
+        name: recipeDetails[0].strMeal,
+        image: recipeDetails[0].strMealThumb }]);
+    } else if (path === 'drinks') {
+      setFavoriteRecipe([{
+        id: recipeDetails[0].idDrink,
+        type: recipeDetails[0].strGlass,
+        category: recipeDetails[0].strCategory,
+        alchoolicOrNot: isAlchoolic,
+        name: recipeDetails[0].strDrink,
+        image: recipeDetails[0].strDrinkThumb }]);
+    }
+    if (isFavorite === false) {
+      setFavoriteRecipe('');
+    }
+  };
+
+  const saveOnClipBoard = () => {
+    clipboardCopy(`http://localhost:3000/${path}/${recipeId}`);
+    setLinkCopied(!linkCopied);
   };
 
   const saveDoneSteps = ({ target }) => {
-    const itens = document.getElementById(target.name);
+    const itens = document.getElementsByName(target.id);
+    console.log(itens);
     if (target.checked === true) {
-      itens.className = 'cutted';
-      if (recipeSteps.includes(target.name)) {
-        const removeBox = recipeSteps.filter((step) => step !== target.name);
+      itens[0].className = 'cutted';
+      if (recipeSteps.includes(target.id)) {
+        const removeBox = recipeSteps.filter((step) => step !== target.id);
         setRecipeSteps(removeBox);
       } else {
-        setRecipeSteps([...recipeSteps, target.name]);
+        setRecipeSteps([...recipeSteps, target.id]);
       }
     } else if (target.checked === false) {
-      itens.className = '';
-      const removeBox = recipeSteps.filter((recipe) => recipe !== target.name);
+      itens[0].className = '';
+      const removeBox = recipeSteps.filter((recipe) => recipe !== target.id);
       setRecipeSteps(removeBox);
     }
   };
-
-  /* const checkLocalStorage = () => {
-    if (localStorage.inProgressRecipes.length !== 0) {
-      setRecipeSteps(JSON.parse(localStorage.getItem('inProgressRecipes')));
-      console.log(recipeSteps);
-    } else if (localStorage.inProgressRecipes.length === 0) {
-      const array = JSON.stringify(
-        localStorage.getItem('inProgressRecipes'),
-      );
-      const arrayParsed = array.map((element) => parseInt(element, 10));
-      setRecipeSteps(arrayParsed);
-    }
-  }; */
-
-  /* const checkLocalStorageBoxes = async () => {
-    await checkLocalStorage();
-    await gettedFromLocalStorage;
-    recipeSteps.forEach((element, index) => {
-      const checkbox = document.getElementsByName(`checkbox ${index}`);
-      console.log(checkbox);
-      checkbox[0].checked = true;
-    });
-  }; */
-  console.log(recipeDetails);
   return (
     <div>
       { path === 'meals'
-        ? (
+        && (
           <div>
-            { recipeDetails.length !== 0 && (
-              <div>
-                <img
-                  src={ recipeDetails[path][0].strMealThumb }
-                  alt="Recipe Pic"
-                  data-testid="recipe-photo"
-                />
-                <h2 data-testid="recipe-title">{recipeDetails[path][0].strMeal}</h2>
-                <h4
-                  data-testid="recipe-category"
-                >
-                  {recipeDetails[path][0].strCategory}
-                </h4>
-                <img
-                  src={ whireHeartIcon }
-                  alt="favorite button"
-                  onClick={ () => console.log('clicou favorite') }
-                  aria-hidden
-                />
-                <img
-                  src={ shareIcon }
-                  alt="share button"
-                  data-testid="share-btn"
-                  onClick={ () => console.log('clicou share') }
-                  aria-hidden
-                />
-                <p data-testid="instructions">{recipeDetails[path][0].strInstructions}</p>
-                <form>
-                  {Ingredients.map((checkbox, index) => (
-                    <div key={ index }>
-                      <input
-                        id={ index }
-                        type="checkbox"
-                        onClick={ saveDoneSteps }
-                        onChange={ validateButton }
-                        name={ `${checkbox} ${Measures[index]}` }
-                      />
-                      <label
-                        htmlFor={ `${checkbox} ${Measures[index]}` }
-                        name={ `label ${index}` }
-                        id={ `${checkbox} ${Measures[index]}` }
-                      >
-                        {`${checkbox} ${Measures[index]}`}
+            {recipeDetails ? (
+              recipeDetails.map((recipe) => (
+                <div key={ recipe.strMeal }>
+                  <img
+                    src={ recipe.strMealThumb }
+                    alt="Recipe Pic"
+                    data-testid="recipe-photo"
+                  />
+                  <h2 data-testid="recipe-title">{recipe.strMeal}</h2>
+                  <h4
+                    data-testid="recipe-category"
+                  >
+                    {recipe.strCategory}
+                  </h4>
+                  <img
+                    src={ isFavorite ? blackHeartIcon : whiteHeartIcon }
+                    alt="favorite button"
+                    onClick={ saveOnFavoriteRecipes }
+                    aria-hidden
+                    data-testid="favorite-btn"
+                  />
+                  <img
+                    src={ shareIcon }
+                    alt="share button"
+                    data-testid="share-btn"
+                    onClick={ saveOnClipBoard }
+                    aria-hidden
+                  />
+                  {linkCopied && <p>Link copied!</p>}
+                  <p data-testid="instructions">{recipe.strInstructions}</p>
+                  <form>
+                    {Ingredients.map((checkbox, index) => (
+                      <div key={ index }>
+                        <label
+                          htmlFor={ `${checkbox} ${Measures[index]}` }
+                          name={ `${checkbox} ${Measures[index]}` }
+                          data-testid={ `${index}-ingredient-step` }
+                        >
+                          <input
+                            type="checkbox"
+                            onClick={ saveDoneSteps }
+                            onChange={ validateButton }
+                            id={ `${checkbox} ${Measures[index]}` }
+                          />
+                          {`${checkbox} ${Measures[index]}`}
+                        </label>
+                      </div>))}
+                  </form>
+                  <button
+                    type="button"
+                    onClick={ finishRecipe }
+                    data-testid="finish-recipe-btn"
+                    disabled={ isDisabled }
+                  >
+                    Finish Recipe
 
-                      </label>
-                    </div>))}
-                </form>
-                <button
-                  type="button"
-                  onClick={ () => console.log('clicou finalizar') }
-                  testid="finish-recipe-btn"
-                  disabled={ isDisabled }
-                >
-                  Finish Recipe
-
-                </button>
-              </div>
-            )}
-          </div>
-        )
-        : (
-
+                  </button>
+                </div>))
+            ) : (<div>oi</div>)}
+          </div>)}
+      {path === 'drinks'
+        && (
           <div>
-            <img
-              src={ recipeDetails[path][0].strMealThumb }
-              alt="Recipe Pic"
-              data-testid="recipe-photo"
-            />
-            <h2 data-testid="recipe-title">{recipeDetails[path][0].strMeal}</h2>
-            <h4
-              data-testid="recipe-category"
-            >
-              {recipeDetails[path][0].strCategory}
+            {recipeDetails ? (
+              recipeDetails.map((recipe) => (
+                <div key={ recipe.strDrink }>
+                  <img
+                    src={ recipe.strDrinkThumb }
+                    alt="Recipe Pic"
+                    data-testid="recipe-photo"
+                  />
+                  <h2 data-testid="recipe-title">{recipe.strDrink}</h2>
+                  <h4
+                    data-testid="recipe-category"
+                  >
+                    {recipe.strCategory}
+                  </h4>
+                  <img
+                    src={ isFavorite ? blackHeartIcon : whiteHeartIcon }
+                    alt="favorite button"
+                    onClick={ saveOnFavoriteRecipes }
+                    aria-hidden
+                    data-testid="favorite-btn"
+                  />
+                  <img
+                    src={ shareIcon }
+                    alt="share button"
+                    data-testid="share-btn"
+                    onClick={ saveOnClipBoard }
+                    aria-hidden
+                  />
+                  {linkCopied && <p>Link copied!</p>}
+                  <p data-testid="instructions">{recipe.strInstructions}</p>
+                  <form>
+                    {Ingredients.map((checkbox, index) => (
+                      <div key={ `${checkbox} ${Measures[index]}` }>
+                        <label
+                          htmlFor={ `${checkbox} ${Measures[index]}` }
+                          name={ `${checkbox} ${Measures[index]}` }
+                          data-testid={ `${index}-ingredient-step` }
+                        >
+                          <input
+                            type="checkbox"
+                            onClick={ saveDoneSteps }
+                            onChange={ validateButton }
+                            id={ `${checkbox} ${Measures[index]}` }
+                          />
+                          {`${checkbox} ${Measures[index]}`}
+                        </label>
+                      </div>))}
+                  </form>
+                  <button
+                    type="button"
+                    onClick={ finishRecipe }
+                    data-testid="finish-recipe-btn"
+                    disabled={ isDisabled }
+                  >
+                    Finish Recipe
 
-            </h4>
-            <img
-              src={ whireHeartIcon }
-              alt="favorite button"
-              onClick={ () => console.log('clicou favorite') }
-              aria-hidden
-            />
-            <img
-              src={ shareIcon }
-              alt="share button"
-              data-testid="share-btn"
-              onClick={ () => console.log('clicou share') }
-              aria-hidden
-            />
-            <p data-testid="instructions">{recipeDetails[path][0].strInstructions}</p>
-            {}
-            <button
-              type="button"
-              onClick={ () => console.log('clicou finalizar') }
-              testid="finish-recipe-btn"
-            >
-              Finish Recipe
-
-            </button>
-          </div>
-        )}
+                  </button>
+                </div>))
+            ) : (<div>oi</div>)}
+          </div>)}
     </div>
   );
 }
